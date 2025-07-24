eadde4fcd9d64a23fa60ef0fde15451bd7f284a9
HyeonJu Son <guswn2521@gmail.com>
2025-07-24 22:41:58 +0900

    player_died 시그널에 game_over

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@export var mob_scene: PackedScene
+@onready var mob_timer: Timer = $Timers/MobTimer
+@onready var player: CharacterBody2D = $player
+
+func game_over():
+	mob_timer.stop()
+	print("game_over")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player.connect("player_died", Callable(self, "game_over"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
