ca2845f1539226b37b03dbaaab1ad0bed1859665
HyeonJu Son <guswn2521@gmail.com>
2025-07-29 00:24:04 +0900

    게임 재시작후 players 변수 재할당

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,21 @@
 extends Node
 
 var dna = 0
-@onready var players = get_tree().root.get_node("Game/Players")
+var players: Node2D = null
+#@onready var players = get_tree().root.get_node("Game/Players")
+
+func _ready() -> void:
+	players = get_tree().root.get_node("Game/Players")
 
 func add_item():
+	print("아이템 먹음")
+	if players == null or !is_instance_valid(players):
+		print("player가 null")
+		players = get_tree().root.get_node("Game/Players")
+		if players == null:
+			print("또 null")
+			return
+	
 	var cnt_players = float(players.get_child_count())
 	dna += 1 / cnt_players
 	print(dna)
