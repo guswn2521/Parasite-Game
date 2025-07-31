2ca78941bba4175c79387d974cf2998bf68f8be3
wonjun <cwjcsk@naver.com>
2025-08-01 01:12:40 +0900

    플레이어 모체 수 표현

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,15 +1,24 @@
 extends Node
 
 signal dna_changed(new_dna)
+signal player_nums_changed(player_nums)
 var players: Node2D = null
 
 var _dna: float = 0
-var dna:float:
+var dna: float:
 	get:
 		return _dna
 	set(value):
 		_dna = value
 		emit_signal("dna_changed", _dna)
+
+var _player_nums: int = 1
+var player_nums: int:
+	get:
+		return _player_nums
+	set(value):
+		_player_nums = value
+		emit_signal("player_nums_changed", _player_nums)
 
 func _ready() -> void:
 	players = get_tree().root.get_node("Game/Players")
@@ -29,5 +38,6 @@ func add_item():
 func use_item():
 	if dna > 0 :
 		dna -= 1
+		player_nums += 1
 		print("dna 사용. 남은 dna : ", dna)
 	
