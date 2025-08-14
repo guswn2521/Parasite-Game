e79c7f8c701ef1f1e9e1cb44e8873d37b667877b
wonjun <cwjcsk@naver.com>
2025-08-15 01:22:45 +0900

    진화 효과

--- scripts/evolution.gd
+++ scripts/evolution.gd
@@ -2,8 +2,7 @@ extends Button
 
 @onready var players_parent: Node2D = $"../../Players"
 var EVOLVED_PLAYER_SCENE = preload("res://scenes/evolved_player.tscn")
-#var duplicate_effect = preload("res://scenes/duplicate_effect.tscn")
-
+var evolution_effect = preload("res://scenes/evolution.tscn")
 signal no_evolution
 
 func _ready() -> void:
@@ -11,17 +10,25 @@ func _ready() -> void:
 	self.focus_mode = Control.FOCUS_NONE
 	self.pressed.connect(evolve_player)
 
-#func spawn_duplicate_effect(player_position:Vector2) -> void:
-	#var duplicate_fx = duplicate_effect.instantiate()
-	#duplicate_fx.global_position = player_position
-	#duplicate_fx.lifetime = 0.25
-	#duplicate_fx.z_index = 6
-	#players_parent.add_child(duplicate_fx)
-	#print("복제 이펙트 시작")
-	#duplicate_fx.emitting = true
-	#await get_tree().create_timer(0.5).timeout
-	#print("복제 이펙트 끝")
-	#duplicate_fx.queue_free()
+func spawn_evolution_effect(player_position:Vector2) -> void:
+	var evolution_fx = evolution_effect.instantiate()
+	evolution_fx.global_position = player_position
+	evolution_fx.z_index = 6 # 잘보이게 앞에두기
+	players_parent.add_child(evolution_fx)	
+	var subviewport = evolution_fx.get_node("SubViewport")
+	var screen_size = DisplayServer.window_get_size()
+	# 진화 이펙트 전체화면
+	subviewport.size = screen_size
+	# 진화 이펙트 배경 투명하게 처리.
+	subviewport.transparent_bg = true
+	# 렌더링 전 항상 클리어되고 투명 배경을 유지 (다른 이펙트 겹칠때)
+	#subviewport.render_target_clear_mode = SubViewport.CLEAR_MODE_ALWAYS
+	var ani_player = subviewport.get_node("VFX_Level_UP").get_node("AnimationPlayer")
+	print("진화 이펙트 시작")
+	ani_player.play("evolution")
+	await get_tree().create_timer(2).timeout
+	print("진화 이펙트 끝")
+	evolution_fx.queue_free()
 
 func evolve_player() -> void:
 	var origin_player = null
@@ -30,7 +37,7 @@ func evolve_player() -> void:
 		origin_player = children[0]
 	var players_count = GameManager.player_nums
 	
-	if players_count==5:
+	if players_count==1:
 		# 기존 진화 전 플레이어 삭제
 		for child in players_parent.get_children():
 			if child is Player:
@@ -41,8 +48,7 @@ func evolve_player() -> void:
 		new_player.position = origin_player.position + Vector2(0,-30)
 		new_player.z_index = 5
 		# 진화 이펙트 실행
-		#spawn_duplicate_effect(new_player.position + Vector2(-13,-20))
-		#await get_tree().create_timer(0.3).timeout
+		spawn_evolution_effect(new_player.position)
 		# 부모 노드(Players)에 새 노드 추가
 		players_parent.add_child(new_player)
 		print("진화 성공!")
