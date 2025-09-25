ee61e51e156a1d9f60998d1c32ccc65d685812ed
wonjun <cwjcsk@naver.com>
2025-09-25 20:11:37 +0900

    dog collision polygon2d flip 되도록 수정

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -3,6 +3,7 @@ extends "res://scripts/monster_base.gd"
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var hit_box_collisionshape: CollisionShape2D = $HitBox/CollisionShape2D
+@onready var collision_polygon_2d: CollisionPolygon2D = $MonsterArea/CollisionPolygon2D
 
 func _ready() -> void:
 	super()
@@ -12,8 +13,10 @@ func _physics_process(delta: float) -> void:
 	# Flip monster
 	if ray_cast_left.is_colliding():
 		flip_player()
+		see_right(direction)
 	if ray_cast_right.is_colliding():
 		flip_player()
+		see_right(direction)
 	# Death
 	if death:
 		hit_box_collisionshape.disabled = true # 죽으면 공격 불가능하게
@@ -28,8 +31,15 @@ func _physics_process(delta: float) -> void:
 			if distance > 0:
 				direction = 1
 				animated_sprite.flip_h = false
+				see_right(true)
 			else:
 				direction = -1
 				animated_sprite.flip_h = true
+				see_right(false)
 	super(delta)
 	
+func see_right(direction:int) -> void:
+	if direction==1:
+		collision_polygon_2d.scale.x = 1
+	else:
+		collision_polygon_2d.scale.x = -1
